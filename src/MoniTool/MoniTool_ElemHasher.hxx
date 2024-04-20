6f5629d50b9ebd45d825f7b7de66376d9c545c11
Code-madao <529208638@qq.com>
2024-04-20 20:31:18 +0800

    trivial

--- src/MoniTool/MoniTool_ElemHasher.hxx
+++ src/MoniTool/MoniTool_ElemHasher.hxx
@@ -24,17 +24,15 @@
 #include <Standard_Integer.hxx>
 class MoniTool_Element;
 
-
 //! ElemHasher defines HashCode for Element, which is : ask a
 //! Element its HashCode !  Because this is the Element itself
 //! which brings the HashCode for its Key
 //!
 //! This class complies to the template given in TCollection by
 //! MapHasher itself
-class MoniTool_ElemHasher 
+class MoniTool_ElemHasher
 {
 public:
-
   DEFINE_STANDARD_ALLOC
 
   //! Returns hash code for the given element, in the range [1, theUpperBound].
@@ -42,35 +40,16 @@ public:
   //! @param theElement the element which hash code is to be computed
   //! @param theUpperBound the upper bound of the range a computing hash code must be within
   //! @return a computed hash code, in the range [1, theUpperBound]
-  Standard_EXPORT static Standard_Integer HashCode (const Handle (MoniTool_Element) & theElement,
-                                                    Standard_Integer                  theUpperBound);
+  Standard_EXPORT static Standard_Integer HashCode(const Handle(MoniTool_Element) & theElement,
+                                                   Standard_Integer theUpperBound);
 
   //! Returns True if two keys are the same.
   //! The test does not work on the Elements themselves but by
   //! calling their methods Equates
-  Standard_EXPORT static Standard_Boolean IsEqual (const Handle(MoniTool_Element)& K1, const Handle(MoniTool_Element)& K2);
-
-
-
+  Standard_EXPORT static Standard_Boolean IsEqual(const Handle(MoniTool_Element) & K1, const Handle(MoniTool_Element) & K2);
 
 protected:
-
-
-
-
-
 private:
-
-
-
-
-
 };
-
-
-
-
-
-
 
 #endif // _MoniTool_ElemHasher_HeaderFile
